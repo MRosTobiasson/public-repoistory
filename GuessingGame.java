19ceed23e3516a25f7cd732db0d4a99517658fc8
MRosTobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2022-11-24 10:18:16 +0100

    finish script

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,7 +2,7 @@ public class GuessingGame {
 
 public static void main(String[] args) {
 
-Guesser g1 = new Guesser(0, 1000);
+Guesser g1 = new Guesser(0, 10);
 	
 	g1.start();
 		}
---
+++ run_game.sh
@@ -0,0 +1,11 @@
+
+
+echo "$USER labb"
+javac GuessingGame.java
+echo "Compiling program...."
+echo "Running game from /Users/mikaeltobiasson/Inlämning1/mikael_rostobiasson_labb"
+java GuessingGame
+echo "Done!"
+echo "Removing class files...."
+rm GuessingGame.class
+rm Guesser.class
